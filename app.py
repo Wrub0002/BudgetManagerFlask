82afa84d46e6195761374bb5afddc99ed8f0e77d
Wrub0002 <Wrub0002@algonquinlive.com>
2025-04-09 14:42:09 -0400

    feat: allow adding income with form and save to file

--- app.py
+++ app.py
@@ -17,12 +17,34 @@ def homepage():
             with open("expenses.txt", "a") as file:
                 file.write(f"{date} | {description} | {amount}$\n")
 
+        # Add Income
         elif form_type == "income":
             date = request.form["date"]
             source = request.form["source"].capitalize()
             amount = request.form["amount"]
             with open("income.txt", "a") as file:
                 file.write(f"{date} | {source} | {amount}$\n")
+
+        # Recent income expenses
+        latest_income = []
+        try:
+            with open("income.txt", "r") as file:
+                lines = file.readlines()
+                last_three = lines[-3:]
+
+                for line in last_three:
+                    parts = line.strip().split(" | ")
+                    if len(parts) == 3:
+                        date = parts[0]
+                        source = parts[1]
+                        amount = parts[2].replace("$", "")
+                        latest_income.append({
+                            "date": date,
+                            "source": source,
+                            "amount": amount
+                        })
+        except FileNotFoundError:
+            pass
 
     # Recent Expenses
     latest_expenses = []
@@ -44,7 +66,7 @@ def homepage():
                 })
 
 
-    return render_template("index.html", latest_expenses=latest_expenses)
+    return render_template("index.html", latest_expenses=latest_expenses, latest_income=latest_income)
 
 @app.route("/expenses")
 def show_expenses():
